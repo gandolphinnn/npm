b77c2d1b8a35cf90fef9f6e2a63d55b5102ed6dd
gandnolphinnn <gandolfiluca03@gmail.com>
2022-05-28 08:53:51 +0200

    fixed controls->key

--- modules/controls/index.js
+++ modules/controls/index.js
@@ -9,25 +9,7 @@ let mouse = {
 		r: false
 	}
 };
-let key = {
-	w: false,
-	up: false,
-	a: false,
-	left: false,
-	s: false,
-	down: false,
-	d: false,
-	right: false,
-	q: false,
-	e: false,
-	r: false,
-	shift: false,
-	ctrl: false,
-	space: false,
-	enter: false,
-	tab: false,
-	esc: false
-};
+let key = {};
 document.addEventListener('contextmenu', e => e.preventDefault()); //? prevent right click menu
 document.addEventListener('mousedown', e => {
 	switch (e.button) {
@@ -55,44 +37,11 @@ document.addEventListener('mousemove', e => {
 document.addEventListener('keydown', e => {
 	if (e.code != 'F5' && e.code != 'F12')
 		e.preventDefault() //? allow to refresh and to open the page inspect
-	switch (e.code) {
-		case 'KeyW': key.w = true; break;
-		case 'ArrowUp': key.up = true; break;
-		case 'KeyA': key.a = true; break;
-		case 'ArrowLeft': key.left = true; break;
-		case 'KeyS': key.s = true; break;
-		case 'ArrowDown': key.down = true; break;
-		case 'KeyD': key.d = true; break;
-		case 'ArrowRight': key.right = true; break;
-		case 'KeyQ': key.q = true; break;
-		case 'KeyE': key.e = true; break;
-		case 'KeyR': key.r = true; break;
-		case 'Space': key.space = true; break;
-		case 'Enter': key.enter = true; break;
-		case 'Tab': key.tab = true; break;
-		case 'Escape': key.esc = true; break;
-		default: break;
-	}
+	let code = e.code.replace('Key', 'K').replace('Digit', 'D').replace('Numpad', 'P').replace('Arrow', '');
+	key[code] = true;
+	console.log(key);
 });
 document.addEventListener('keyup', e => {
-	switch (e.code) {
-		case 'KeyW': key.w = false; break;
-		case 'ArrowUp': key.up = false; break;
-		case 'KeyA': key.a = false; break;
-		case 'ArrowLeft': key.left = false; break;
-		case 'KeyS': key.s = false; break;
-		case 'ArrowDown': key.down = false; break;
-		case 'KeyD': key.d = false; break;
-		case 'ArrowRight': key.right = false; break;
-		case 'LShift': case 'RShift': key.shift = false; break;		
-		case 'LControl': case 'RControl': key.ctrl = false; break;		
-		case 'KeyQ': key.q = false; break;
-		case 'KeyE': key.e = false; break;
-		case 'KeyR': key.r = false; break;
-		case 'Space': key.space = false; break;
-		case 'Enter': key.enter = false; break;
-		case 'Tab': key.tab = false; break;
-		case 'Escape': key.esc = false; break;
-		default: break;
-	}
+	let code = e.code.replace('Key', 'K').replace('Digit', 'D').replace('Numpad', 'P').replace('Arrow', '');
+	delete key[code];
 });
